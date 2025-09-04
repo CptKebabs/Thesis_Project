fedb9b5bb72d34824adff371e6b242ac1ba9e3c2
CptKebabs <Guven.Gemikonakli@students.mq.edu.au>
2025-09-04 15:56:07 +1000

    more repo setup

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 # Main program, will utilise the other subsystems together
 
 #Process will be as follows:
-    #loop for all pairs of images(
+    #loop for all pairs of images (first get it working for one then repeat for all in input folder)(
         #preprocess with B3MD (top and bottom)
 
-        #Generate point clouds with DepthAnythingV2 (top and bottom)
+        #Generate point clouds with DepthAnythingV2 and scale them using a reference object in frame (top and bottom)
 
         #run segmentation algorithm YOLO(top and bottom)
 
@@ -13,7 +13,11 @@
         #merge top and bottom coordinate frames (rotation and translation)
 
         #reproject into each perspective (top and bottom)
+
+        #write point cloud and segmentations to output folder
     #)end loop
+
+    #have something here to compare the original segmentations with the ones this program has created
 
 #Assumptions:
     
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Thesis_Project
 3D Reconstruction Program
 Guven Gemikonakli
+
+## Requirements:
+- Python Version:
+- OpenCV Version:
+- Etc...
+
+
+## Current todos
+- setup DepthAnythingV2, YOLO and B3MD
