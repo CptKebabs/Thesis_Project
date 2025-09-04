09b093ca76620511728146ef63116a08f0e09b37
cptkebabs <Guven.Gemikonakli@students.mq.edu.au>
2025-09-04 13:06:00 +1000

    initial file structure

--- Main.py
+++ Main.py
@@ -1,21 +1,24 @@
 # Main program, will utilise the other subsystems together
 
 #Process will be as follows:
+    #loop for all pairs of images(
+        #preprocess with B3MD (top and bottom)
 
-    #preprocess first (top and bottom)
+        #Generate point clouds with DepthAnythingV2 (top and bottom)
 
-    #Generate point clouds (top and bottom)
+        #run segmentation algorithm YOLO(top and bottom)
 
-    #run segmentation algorithm (top and bottom)
+        #merge point clouds with segmentation algorithm (top and bottom)
 
-    #merge point clouds with segmentation algorithm (top and bottom)
+        #merge top and bottom coordinate frames (rotation and translation)
 
-    #merge top and bottom coordinate frames
-
-    #reproject into each perspective (top and bottom)
+        #reproject into each perspective (top and bottom)
+    #)end loop
 
 #Assumptions:
     
     #needs a trained segmentation algorithm
 
     #will need a way to compare to segmentation algorithm alone
+
+    # images need an object of known size to exist within it to be able to determine scale
---
+++ PointcloudGenerator.py
@@ -0,0 +1,11 @@
+#use depthAnythingV2 to generate the depth maps, and convert them to a scaled point cloud here
+#https://www.youtube.com/watch?v=vGr8Bg2Fda8
+
+
+#call depthAnythingV2 model on image
+
+
+
+
+
+#output scaled point cloud
